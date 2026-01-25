1857b5984949ac18279209f490b2c23ab8ef6959
revaria <tanchunliang2003@gmail.com>
2026-01-25 17:36:15 +0800

    Replace Duke with Caesar, add Greet and Exit lines

---
+++ src/main/java/Caesar.java
@@ -0,0 +1,15 @@
+public class Caesar {
+    public static void main(String[] args) {
+        String logo = " _____ \n"
+                + "/  ___\\ _____  _____  _____  __ __  _____ \n"
+                + "| |    /     |/ / _ \\/ /___\\/     |/  ___\\ \n"
+                + "| !    | [@] | <  __/\\___\\ \\| [@] || | \n"
+                + "| \\___ \\____,?\\_\\___|\\_____/\\_ __,?|_|\n"
+                + "\\_____/ \n";
+        System.out.println("Hello Brutus, I'm\n" + logo);
+
+        System.out.println("What shall we conquer today?");
+        System.out.println("__________________________________________________________\n");
+        System.out.println("Goodbye Brutus, the stars speak of our inevitable reunion.");
+    }
+}
