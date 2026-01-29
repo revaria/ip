1adee739b0d9f11b9475fd9fd933fd099c8e62f4
revaria <tanchunliang2003@gmail.com>
2026-01-30 01:23:11 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Caesar {
@@ -13,14 +14,29 @@ public class Caesar {
         System.out.println("What shall we conquer today?");
         System.out.println("__________________________________________________________");
 
+        String[] tasks = new String[100];
+        int task_count = 0;
+        String bye_String = "bye";
+        String list_String = "list";
+
         String line;
         Scanner in = new Scanner(System.in);
         while(true) {
             line = in.nextLine();
-            if (line.contains("bye")) {
+            if (line.trim().equals(bye_String)) {
                 break;
             }
-            System.out.println("You said: " + line);  
+            else if(line.trim().equals(list_String)){
+                String[] recorded_tasks = Arrays.copyOf(tasks, task_count);
+                for (int j = 0; j < task_count; j++) {
+                    System.out.println((j+1) + ": " + recorded_tasks[j]);
+                }
+            }
+            else {
+                tasks[task_count] = line;
+                System.out.println("added: " + line); 
+                task_count++;
+            } 
             System.out.println("What else shall we conquer today?");
             System.out.println("__________________________________________________________");
                                          
